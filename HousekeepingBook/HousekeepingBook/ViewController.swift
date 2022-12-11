260f4a2720675ba64a12f6d8822f0a7e2200db15
Mitti09 <develop.application01@gmail.com>
2022-12-11 17:19:30 +0900

    🎨カレンダー、tableviewを表示

--- HousekeepingBook/HousekeepingBook/ViewController.swift
+++ HousekeepingBook/HousekeepingBook/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FSCalendarDataSource,FSCalendarDelegate,UITableViewDataSource,UITableViewDelegate {
+
+//** IBOutlet
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+//** LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+//** Propatiy
+    var list = ["月曜","火曜","水曜"]
+    
+//** MARK ** private func
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel!.text = list[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
     }
 
 
