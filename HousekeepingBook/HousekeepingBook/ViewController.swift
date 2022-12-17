12625047f3547bc257ba13f404109c40629ab98b
Mitti09 <develop.application01@gmail.com>
2022-12-17 16:28:59 +0900

    ✨mainviewに表示されるcellを追加

--- HousekeepingBook/HousekeepingBook/ViewController.swift
+++ HousekeepingBook/HousekeepingBook/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import FSCalendar
 
-class ViewController: UIViewController,FSCalendarDataSource,FSCalendarDelegate,UITableViewDataSource,UITableViewDelegate {
+class ViewController: UIViewController,FSCalendarDataSource,FSCalendarDelegate {
 
 //** IBOutlet
     @IBOutlet private weak var calendar: FSCalendar!
@@ -22,6 +22,8 @@ class ViewController: UIViewController,FSCalendarDataSource,FSCalendarDelegate,U
         super.viewDidLoad()
         initDateLabel()
         
+        tableView.register(UINib(nibName: "CalendarTableViewCell", bundle: nil), forCellReuseIdentifier: "CalendarTableViewCell")
+        
         // Do any additional setup after loading the view.
     }
 
@@ -30,15 +32,15 @@ class ViewController: UIViewController,FSCalendarDataSource,FSCalendarDelegate,U
     let df = DateFormatter()
     
 //** MARK ** private func
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel!.text = list[indexPath.row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
-    }
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//        cell.textLabel!.text = list[indexPath.row]
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return list.count
+//    }
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         df.dateFormat = "yyyy/MM/dd"
@@ -55,6 +57,39 @@ extension ViewController {
         df.dateFormat = "yyyy/MM/dd"
         dateLabel.text = df.string(from: Date())
         }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    // セクションの個数を返す
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+
+        // セクションごとにセルの個数を返す
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+
+        // セルの中身を返す
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell: CalendarTableViewCell = tableView.dequeueReusableCell(withIdentifier: "CalendarTableViewCell") as! CalendarTableViewCell
+            return cell
+        }
+
+        // セルの高さを返す
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 50
+        }
+    }
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("section: \(indexPath.section) index: \(indexPath.row)")
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        return
     }
 }
 
